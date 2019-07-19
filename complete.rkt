07463babe0f93e890423a09557433ed22c78ccba
solawing <316786359@qq.com>
2019-07-19 10:41:31 +0800

    commnet:

--- complete.rkt
+++ complete.rkt
@@ -7,6 +7,7 @@
 
 ;; for now, let's just be happy with a `starts-with` completion.
 ;; TODO: fuzzy completion (or at least, a bit smarter than this)
+;; if return full candidates, will frozon slimv..
 (define (simple-complete pattern)
   (let ([candidates (filter 
                       (curry string-prefix-ci? pattern) 
